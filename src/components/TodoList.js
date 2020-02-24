1a4f5454152bb1da63f503b9fa04efe0ebc3c248
M M Mahmudul Hassan <sazibsust10@gmail.com>
2020-02-25 00:23:28 +0100

    Creating UI

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import TodoItem from "./TodoItem";
+export default class TodoList extends Component {
+  render() {
+    return (
+        <ul className="list-group my-5">
+            <h3 className="text-capitalize text-center">
+                Task List
+            </h3>
+            <TodoItem />
+            <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">Clear List</button>
+        </ul>
+    );
+  }
+}
