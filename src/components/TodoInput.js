a4427335ff598085be2a999c1de5c0a56c4dfa86
M M Mahmudul Hassan <sazibsust10@gmail.com>
2020-02-25 11:59:01 +0100

    Adding functionality

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
       <div className="card card-body my-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <div className="input-group-prepend">
               <div className="input-group-text bg-primary text-white">
@@ -15,9 +16,17 @@ export default class TodoInput extends Component {
               type="text"
               className="form-control"
               placeholder="Add a todo item"
+              value={item}
+              onChange={handleChange}
             />
           </div>
-          <button type="submit" className="btn btn-block btn-primary mt-3">Add Task</button>
+          <button type="submit" className={
+            editItem ?
+            "btn btn-block btn-success mt-3" :
+            "btn btn-block btn-primary mt-3"
+          }>
+            {editItem ? "Edit Taks" : "Add Task"}
+          </button>
         </form>
       </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,14 +3,29 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-        <ul className="list-group my-5">
-            <h3 className="text-capitalize text-center">
-                Task List
-            </h3>
-            <TodoItem />
-            <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">Clear List</button>
-        </ul>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">Task List</h3>
+        {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
